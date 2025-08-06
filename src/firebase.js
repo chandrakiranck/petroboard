fe486e35e87974c9633927154aaa7e5e50251d1e
Chandrakiranreddy Gudipally <chandrakiranreddyg@gmail.com>
2025-08-06 11:51:20 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
-// Replace with your Firebase project config from console
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDtibkYYC9TNqBQYmats1G5Qk51z17AKZQ",
+  authDomain: "petroboard-b787b.firebaseapp.com",
+  projectId: "petroboard-b787b",
+  storageBucket: "petroboard-b787b.appspot.com",
+  messagingSenderId: "322692273978",
+  appId: "1:322692273978:web:b06129abad63529ec26203",
+  measurementId: "G-GL9545T42M",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
