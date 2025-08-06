ee9e84c498bd6b6fc6546c50a951310eada28e5f
Chandrakiranreddy Gudipally <chandrakiranreddyg@gmail.com>
2025-08-06 11:42:38 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Replace with your Firebase project config from console
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
