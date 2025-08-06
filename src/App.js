e973732340e5fa74e45d606a16204804a9ff336c
Chandrakiranreddy Gudipally <chandrakiranreddyg@gmail.com>
2025-08-06 12:39:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import Dashboard from "./components/Dashboard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
