e28f321957eee4f312feeeed72ce5964305ece4b
Chandrakiranreddy Gudipally <chandrakiranreddyg@gmail.com>
2025-08-06 12:27:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // ✅ Add this
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtibkYYC9TNqBQYmats1G5Qk51z17AKZQ",
@@ -14,7 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app); // ✅ Add this export
+export { db };
